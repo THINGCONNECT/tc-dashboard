ce766c4790d55ad78b082d016137f248d8d9327b
David Kim <davidk@halopowered.com>
2015-08-18 19:34:52 -0400

    Added an error alert to new sim page

--- client/js/controllers/NewSimCtrl.js
+++ client/js/controllers/NewSimCtrl.js
@@ -1,19 +1,30 @@
 var module = require('./module');
 module.controller('NewSimCtrl', function($scope, UserState, $state, $mdDialog) {
   $scope.hideForm = false;
+
   UserState.loggedIn().then(function(user){
-    $scope.verifySim = function() {
-      // $state.go("verify-sim", {id: $scope.sim.simId});
+    $scope.verifySim = function(ev) {
       UserState.newSim($scope.sim).then(function(sim){
-        console.log("WUT: ", sim);
         if(sim){
           $scope.verifyCode = sim.verifyCode;
           $scope.hideForm = true;
         }else{
-          console.log("wtf ", sim);
+          console.log("something bad happened");
         }
       }).catch(function(err){
-        console.log(err);
+        console.log(err.data.message);
+        $mdDialog.show(
+          $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#container')))
+            .clickOutsideToClose(true)
+            .title('Error')
+            .content(err.data.message)
+            .ariaLabel('Alert Dialog Demo')
+            .ok('Ok')
+            .targetEvent(ev)
+        );
+        
+        $scope.hideForm = false;
       });
 
     };
