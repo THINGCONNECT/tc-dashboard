ee85fa02d0824c4b48e3fdd3bd739d963e015c9d
Adam Lee <adam@ninjastack.com>
2015-11-11 15:26:37 -0500

    probably this

--- server/lib/routes/api/sim/verify.js
+++ server/lib/routes/api/sim/verify.js
@@ -5,26 +5,29 @@ var router = require('express').Router({
 
 var Verify = mongoose.model('Verify');
 
-router.route('/').post(function (req, res) {
-  console.log("Verification");
-  var body = req.body;
-  var simId = body.simId;
-  var user = req.user;
-  
-  Verify.createVerification(simId, user, function(err, verification){
-    if(!err){
-      console.log("Verification Created ", verification.verifyCode);
-      return res.ok({verifyCode: verification.verifyCode});
-    } else {
-      console.log("Verification not created", simId);
-      return res.error(500, err);
-    }
-  });
-});
+router.route('/')
 
-router.route('/').get(function (req, res) {
-  console.log("Verification");
-  return res.ok("works");
-});
+.post(function(req, res) {
+    console.log("Verification");
+    var body = req.body;
+    var simId = body.simId;
+    var user = req.user;
+
+    Verify.createVerification(simId, user, function(err, verification) {
+      if (!err) {
+        console.log("Verification Created ", verification.verifyCode);
+        return res.ok({
+          verifyCode: verification.verifyCode
+        });
+      } else {
+        console.log("Verification not created", simId);
+        return res.error(500, err);
+      }
+    });
+  })
+  .get(function(req, res) {
+    console.log("Verification");
+    return res.ok("works");
+  });
 
 module.exports = router;
