0a74211a5a0417fd8b371ac7356c67ad315d529f
David Kim <davidk@halopowered.com>
2016-05-22 17:39:32 -0400

    Updated send endpoint and package json

--- server/lib/routes/api/sim/_sim/send/root.js
+++ server/lib/routes/api/sim/_sim/send/root.js
@@ -2,15 +2,27 @@ var mongoose = require('mongoose');
 var router = require('express').Router({
   mergeParams: true
 });
+var Sim = mongoose.model('Sim');
 var simHandler = require('lib/simHandler');
 
 //This endpoint is for testing purposes
 router.route('/').get(function(req, res) {
   var simId = req.params.sim;
-  var payload = req.body.payload;
-  payload = "TEST!";
-  simHandler.sendMessage(simId, payload, function(err) {
-    res.ok(true);
+  var payload = req.query.payload;
+  var user = req.user;
+  console.log(simId);
+
+  // Check for API key
+  Sim.findOne({simId: simId, owner:user, verified: true}, function(err, sim) {
+    if(!err && sim){
+      simHandler.sendMessage(simId, payload, function(err, msg) {
+        if(err)
+          return res.error(500);
+        res.ok(true);
+      });
+    }else{
+      res.error(404);
+    }
   });
 });
 
