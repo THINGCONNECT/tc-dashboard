8474a8fb158adb6c48329fd9b1b69b239ddfa5cc
David Kim <davidk@halopowered.com>
2016-08-24 14:32:15 -0400

    Fixed remove error

--- server/lib/models/Sim.js
+++ server/lib/models/Sim.js
@@ -43,9 +43,10 @@ SimSchema.methods.verify = function(str, cb) {
 };
 
 SimSchema.methods.disown = function(cb){
-  this.model('User').findByIdAndUpdate(this.owner._id, {$pull: {sims: this._id}}).exec();
+  this.model('User').findByIdAndUpdate(this.owner, {$pull: {sims: this._id }}).exec();
   this.model('Verify').findOne({simId: this.simId}).exec(function(err, verify){
-   verify.remove();
+    if(verify)
+      verify.remove();
   });
   this.verified = false;
   this.save(function(err){
